7af35b2575e380196ebae8a7f2c33023a178b622
Vlad Cherniavsky <vlad.cherniavsky@esw518.ES.local>
2015-12-18 17:06:41 +0300

    1

--- server/app.js
+++ server/app.js
@@ -1,3 +1,16 @@
 /**
  * Created by vlad.cherniavsky on 18.12.2015.
  */
+
+var  express = require('express');
+
+var app=express();
+
+
+app.get('/', function(req,res){
+    res.send("Hello");
+});
+
+app.listen(3000,function(){
+    console.log('app is running on port 3000');
+});
