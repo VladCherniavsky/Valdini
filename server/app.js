6779dd128ea91436b1f7ddfd8f067e973508b52c
Vlad Cherniavsky <vlad.cherniavsky@esw518.ES.local>
2015-12-23 18:06:18 +0300

    11

--- server/app.js
+++ server/app.js
@@ -2,15 +2,22 @@
  * Created by vlad.cherniavsky on 18.12.2015.
  */
 
-var  express = require('express');
+var express=require('express'),
+	db=require('./libs/mongoose'),
+	passport = require('passport');
 
 var app=express();
-
+app.use(express.static(__dirname+'../../public'));
 
 app.get('/', function(req,res){
-    res.send("Hello");
+	res.send("Hello from app.js");
 });
 
-app.listen(3000,function(){
-    console.log('app is running on port 3000');
+
+
+
+app.listen(3000, function(){
+	console.log("server is running on port 3000");
 });
+
+
