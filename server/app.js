f870e0ae34eaaee300d805048a292824366c0366
Vlad Cherniavsky <vlad.cherniavsky@esw518.ES.local>
2015-12-28 14:38:27 +0300

    login page

--- server/app.js
+++ server/app.js
@@ -1,43 +1,46 @@
 /**
  * Created by vlad.cherniavsky on 18.12.2015.
  */
-
-var express     = require('express'),
-    db          = require('./lib/mongoose'),
-    passport    = require('passport'),
-    bodyParser  = require('body-parser'),
-    morgan      = require('morgan'),
-    jwt         = require('jsonwebtoken'),
-    config      = require('./config/config'),
-    User        = require('./models/userModel');
+ /*jslint white: true */
 
 
 
-var app=express();
+ var express     = require('express'),
+   // db          = require('./lib/mongoose'),
+   passport    = require('passport'),
+   bodyParser  = require('body-parser'),
+   morgan      = require('morgan'),
+   jwt         = require('jsonwebtoken'),
+   // config      = require('./config/config'),
+   User        = require('./models/userModel');
 
 
-var apiRoutes=express.Router();
 
-app.use(express.static(__dirname+'../../public'));
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
-app.use(morgan('dev'));
+   var app=express();
 
 
+   var apiRoutes=express.Router();
 
-apiRoutes.get('/',function(req,res){
-   
+   app.use(express.static(__dirname+'../../public'));
+   app.use(bodyParser.urlencoded({extended:false}));
+   app.use(bodyParser.json());
+   app.use(morgan('dev'));
+
+
+
+   apiRoutes.get('/',function(req,res){
+     
     res.json({message:'Welcome to the coolest api in the world'});
 });
-apiRoutes.get('/users', function(req,res){
-   
-    User.find({},function(err,users){
+   apiRoutes.get('/users', function(req,res){
+     'user strict';
+     User.find({},function(err,users){
         res.json(users);
     });
-});
+ });
 
-apiRoutes.post('/authenticate', function(req,res){
-   
+   apiRoutes.post('/authenticate', function(req,res){
+     
     User.findOne({
         username:req.body.name
     },function(err,user){
@@ -63,7 +66,7 @@ apiRoutes.post('/authenticate', function(req,res){
     });
 });
 
-apiRoutes.use(function(req,res,next){
+   apiRoutes.use(function(req,res,next){
     console.log('check token');
     var token = req.body.token ||req.query.token || req.headers['x-access-token'];
     if(token){
@@ -88,36 +91,37 @@ apiRoutes.use(function(req,res,next){
 });
 
 
-app.use('/api',apiRoutes);
+   app.use('/api',apiRoutes);
 
-app.get('/', function(req,res){
- 
+   app.get('/', function(req,res){
+       
     res.send("Hello from app.js");
 });
 
-app.get('/setup', function(req,res){
-   
-        var vlad = new  User({
+   app.get('/setup', function(req,res){
+     'use strict';
+     var vlad = new  User({
         username: "vlad",
         password: "vlad",
         admin:true
-        });
-    vlad.save(function(err){
+    });
+     vlad.save(function(err){
         if(err){
             throw err;
         }
 
         console.log('User saves successfully');
-    res.json({success:true});
-	});
-});
+        res.json({success:true});
+    });
+ });
 
 
 
 
 
-app.listen(3000, function(){
-	console.log("server is running on port 3000");
+   app.listen(3000, function(){
+    'use strict';
+    console.log("server is running on port 3000");
 });
 
 
