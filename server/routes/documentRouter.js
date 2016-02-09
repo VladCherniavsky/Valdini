007d6ba57460e847af86aae4f24748058981b4d8
Vlad Cherniavsky <Vlad.Cherniavsky@ES.local>
2016-02-09 20:27:03 +0300

    parsing files is working but it didnt save to db

--- server/routes/documentRouter.js
+++ server/routes/documentRouter.js
@@ -1,17 +1,28 @@
 var express = require('express'),
     multer  = require('multer'),
     fandomsCtrl = require('../controllers/fandomsCtrl');
+var fs = require('fs');
+var parse = require('csv-parse');
+
+
+var parser = parse({skip_empty_lines : true, delimiter: ','});
 
 var storage = multer.memoryStorage();
-var upload = multer({ storage: storage }),
+var upload = multer({ dest: 'uploads/' }),
     router = express.Router();
 
 
 router.post('/document/upload', upload.single('file'), function (req, res, next){
-    console.log('req.files', req.files);
+
     console.log('req.file', req.file);
     console.log('req.body', req.body);
+    fs.createReadStream(req.file.path).pipe(parser)
+        .on('data', function(line) {
+        console.log(line);
+        });
+
     res.status(200).json(req.file);
+
 });
 
 module.exports = router;
